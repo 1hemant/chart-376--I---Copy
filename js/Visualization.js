d4a99cfed36a99af95ff900818846b4002896ba8
1hemant <demonvsgod@gmail.com>
2016-07-06 18:00:57 +0530

    Functioning
    Without filter, associativity, drilldown and widgets

---
+++ js/Visualization.js
@@ -0,0 +1,404 @@
+var visualData = {
+  "version": {
+    "pointer": "10.0.1",
+    "name": "vanilla"
+  },
+  "status": {
+    "code": "200",
+    "value": "success"
+  },
+  "data": {
+    "513": {
+      "configuration": {
+        "chartType": "Chart",
+        "result": {
+          "subtitle": {
+            "text": "",
+            "x": -35,
+            "y": 20,
+            "style": {
+              "fontSize": "11px",
+              "fontWeight": "normal",
+              "color": "rgba(2, 9, 29, 0.76)",
+              "textAlign": "left"
+            }
+          },
+          "legend": {
+            "enabled": false
+          },
+          "xAxis": [
+            {
+              "labels": {
+                "enabled": false
+              }
+            }
+          ],
+          "yAxis": {
+            "title": {
+              "text": null
+            },
+            "gridLineWidth": 0,
+            "labels": {
+              "enabled": false
+            }
+          },
+          "plotOptions": {
+            "series": {
+              "stacking": "percent",
+              "pointWidth": 10,
+              "dataLabels": {
+                "enabled": true,
+                "color": "#000",
+                "x": 20,
+                "y": -12,
+                "formatter": "function (){return this.y<1e3?this.y:(this.y/1e3).toFixed(0)+\"K\"}",
+                "style": {
+                  "fontSize": "12px",
+                  "fontFamily": "Tahoma sans-serif",
+                  "fontWeight": "normal"
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    "516": {
+      "configuration": {
+        "chartType": "Chart",
+        "result": {
+          "subtitle": {
+            "text": "",
+            "x": -35,
+            "y": 20,
+            "style": {
+              "fontSize": "11px",
+              "fontWeight": "normal",
+              "color": "rgba(2, 9, 29, 0.76)",
+              "textAlign": "left"
+            }
+          },
+          "legend": {
+            "enabled": false
+          },
+          "xAxis": [
+            {
+              "labels": {
+                "enabled": false
+              }
+            }
+          ],
+          "yAxis": {
+            "title": {
+              "text": null
+            },
+            "gridLineWidth": 0,
+            "labels": {
+              "enabled": false
+            }
+          },
+          "plotOptions": {
+            "series": {
+              "stacking": "percent",
+              "pointWidth": 10,
+              "dataLabels": {
+                "enabled": true,
+                "color": "#000",
+                "x": 20,
+                "y": -12,
+                "formatter": "function (){return this.y<1e3?this.y:(this.y/1e3).toFixed(0)+\"K\"}",
+                "style": {
+                  "fontSize": "12px",
+                  "fontFamily": "Tahoma sans-serif",
+                  "fontWeight": "normal"
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    "518": {
+      "configuration": {
+        "chartType": "Chart",
+        "result": {
+          "subtitle": {
+            "text": "",
+            "x": -35,
+            "y": 20,
+            "style": {
+              "fontSize": "11px",
+              "fontWeight": "normal",
+              "color": "rgba(2, 9, 29, 0.76)",
+              "textAlign": "left"
+            }
+          },
+          "legend": {
+            "enabled": false
+          },
+          "xAxis": [
+            {
+              "labels": {
+                "enabled": false
+              }
+            }
+          ],
+          "yAxis": {
+            "title": {
+              "text": null
+            },
+            "gridLineWidth": 0,
+            "labels": {
+              "enabled": false
+            }
+          },
+          "plotOptions": {
+            "series": {
+              "stacking": "percent",
+              "pointWidth": 10,
+              "dataLabels": {
+                "enabled": true,
+                "color": "#000",
+                "x": 20,
+                "y": -12,
+                "formatter": "function (){return this.y<1e3?this.y:(this.y/1e3).toFixed(0)+\"K\"}",
+                "style": {
+                  "fontSize": "12px",
+                  "fontFamily": "Tahoma sans-serif",
+                  "fontWeight": "normal"
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    "520": {
+      "configuration": {
+        "chartType": "Chart",
+        "result": {
+          "subtitle": {
+            "text": "",
+            "x": -35,
+            "y": 20,
+            "style": {
+              "fontSize": "11px",
+              "fontWeight": "normal",
+              "color": "rgba(2, 9, 29, 0.76)",
+              "textAlign": "left"
+            }
+          },
+          "legend": {
+            "enabled": false
+          },
+          "xAxis": [
+            {
+              "labels": {
+                "enabled": false
+              }
+            }
+          ],
+          "yAxis": {
+            "title": {
+              "text": null
+            },
+            "gridLineWidth": 0,
+            "labels": {
+              "enabled": false
+            }
+          },
+          "plotOptions": {
+            "series": {
+              "stacking": "percent",
+              "pointWidth": 10,
+              "dataLabels": {
+                "enabled": true,
+                "color": "#000",
+                "x": 20,
+                "y": -12,
+                "formatter": "function (){return this.y<1e3?this.y:(this.y/1e3).toFixed(0)+\"K\"}",
+                "style": {
+                  "fontSize": "12px",
+                  "fontFamily": "Tahoma sans-serif",
+                  "fontWeight": "normal"
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    "526": {
+      "configuration": {
+        "chartType": "Chart",
+        "result": {
+          "yAxis": [
+            {
+              "min": 0,
+              "gridLineWidth": 0,
+              "title": {
+                "text": "Revenue Units"
+              }
+            },
+            {
+              "min": 0,
+              "gridLineWidth": 0,
+              "title": {
+                "text": "Percentage"
+              },
+              "opposite": "true"
+            },
+            {
+              "min": 0,
+              "gridLineWidth": 0,
+              "title": {
+                "text": ""
+              }
+            }
+          ],
+          "legend": {
+            "enabled": "true"
+          },
+          "tooltip": {
+            "shared": true
+          },
+          "plotOptions": {
+            "column": {
+              "stacking": "normal"
+            },
+            "series": {
+              "cursor": "pointer"
+            }
+          }
+        }
+      }
+    },
+    "556": {
+      "configuration": {
+        "chartType": "Chart",
+        "result": {
+          "yAxis": [
+            {
+              "min": 0,
+              "gridLineWidth": 0,
+              "title": {
+                "text": ""
+              }
+            },
+            {
+              "min": 0,
+              "gridLineWidth": 0,
+              "title": {
+                "text": ""
+              },
+              "opposite": "true"
+            },
+            {
+              "min": 0,
+              "gridLineWidth": 0,
+              "title": {
+                "text": ""
+              }
+            }
+          ],
+          "legend": {
+            "enabled": "true"
+          },
+          "tooltip": {
+            "shared": true
+          },
+          "plotOptions": {
+            "column": {
+              "stacking": "normal"
+            },
+            "series": {
+              "cursor": "pointer"
+            }
+          }
+        }
+      }
+    },
+    "558": {
+      "configuration": {
+        "chartType": "Chart",
+        "result": {
+          "yAxis": [
+            {
+              "min": 0,
+              "gridLineWidth": 0,
+              "title": {
+                "text": ""
+              }
+            },
+            {
+              "min": 0,
+              "gridLineWidth": 0,
+              "title": {
+                "text": ""
+              },
+              "opposite": "true"
+            },
+            {
+              "min": 0,
+              "gridLineWidth": 0,
+              "title": {
+                "text": ""
+              }
+            }
+          ],
+          "legend": {
+            "enabled": "true"
+          },
+          "tooltip": {
+            "shared": true
+          },
+          "plotOptions": {
+            "column": {
+              "stacking": "normal"
+            },
+            "series": {
+              "cursor": "pointer"
+            }
+          }
+        }
+      }
+    },
+    "560": {
+      "configuration": {
+        "chartType": "Chart",
+        "result": {
+          "yAxis": [
+            {
+              "min": 0,
+              "labels": {
+                "enabled": false
+              },
+              "title": {
+                "text": "#Calls Handled"
+              },
+              "gridLineWidth": 0
+            }
+          ],
+          "plotOptions": {
+            "column": {
+              "series": {
+                "pointWidth": 1
+              },
+              "stacking": "normal",
+              "cursor": "pointer",
+              "dataLabels": {
+                "enabled": false,
+                "format": "<b>{point.name}</b>: {point.percentage:.0f} %"
+              },
+              "showInLegend": true
+            }
+          },
+          "tooltip": {
+            "backgroundColor": "rgba(255, 255, 255, .4)"
+          }
+        }
+      }
+    }
+  },
+  "error": false
+};
---
+++ js/Workboard.js
@@ -0,0 +1,488 @@
+var workboardData = {
+  "version": {
+    "pointer": "10.0.1",
+    "name": "vanilla"
+  },
+  "status": {
+    "code": "200",
+    "value": "successfull"
+  },
+  "data": {
+    "493": {
+      "configuration": {
+        "type": "filter-element",
+        "name": "pWeekEnd",
+        "itemType": "select",
+        "configuration": {},
+        "params": {
+          "name": "pWeekEnd"
+        },
+        "class": "col-md-2 col-lg-2 col-xs-2",
+        "affected": []
+      }
+    },
+    "494": {
+      "configuration": {
+        "type": "filter-element",
+        "name": "pDistrict",
+        "itemType": "multiselect",
+        "configuration": {},
+        "params": {
+          "name": "pDistrict"
+        },
+        "class": "col-md-2 col-lg-2 col-xs-2",
+        "affected": []
+      }
+    },
+    "495": {
+      "configuration": {
+        "type": "filter-element",
+        "name": "pCity",
+        "itemType": "multiselect",
+        "configuration": {},
+        "params": {
+          "name": "pCity"
+        },
+        "class": "col-md-2 col-lg-2 col-xs-2",
+        "affected": []
+      }
+    },
+    "501": {
+      "configuration": {
+        "type": "filter-element",
+        "name": "pBCF",
+        "itemType": "multiselect",
+        "configuration": {},
+        "params": {
+          "name": "pBCF"
+        },
+        "class": "col-md-2 col-lg-2 col-xs-2",
+        "affected": []
+      }
+    },
+    "502": {
+      "configuration": {
+        "type": "filter-element",
+        "name": "pLocationID",
+        "itemType": "multiselect",
+        "configuration": {},
+        "params": {
+          "name": "pLocationID"
+        },
+        "class": "col-md-2 col-lg-2 col-xs-2",
+        "affected": []
+      }
+    },
+    "503": {
+      "configuration": {
+        "type": "filter-element",
+        "name": "pBTS",
+        "itemType": "multiselect",
+        "configuration": {},
+        "params": {
+          "name": "pBTS"
+        },
+        "class": "col-md-2 col-lg-2 col-xs-2",
+        "affected": []
+      }
+    },
+    "504": {
+      "configuration": {
+        "type": "filter-element",
+        "name": "pCallUsage",
+        "itemType": "multiselect",
+        "configuration": {},
+        "params": {
+          "name": "pCallUsage"
+        },
+        "class": "col-md-2 col-lg-2 col-xs-2",
+        "affected": []
+      }
+    },
+    "505": {
+      "configuration": {
+        "type": "filter-element",
+        "name": "pCalledRegion",
+        "itemType": "multiselect",
+        "configuration": {},
+        "params": {
+          "name": "pCalledRegion"
+        },
+        "class": "col-md-2 col-lg-2 col-xs-2",
+        "affected": []
+      }
+    },
+    "506": {
+      "configuration": {
+        "type": "filter-element",
+        "name": "pTimeOfDay",
+        "itemType": "multiselect",
+        "configuration": {},
+        "params": {
+          "name": "pTimeOfDay"
+        },
+        "class": "col-md-2 col-lg-2 col-xs-2",
+        "affected": []
+      }
+    },
+    "507": {
+      "configuration": {
+        "type": "filter-element",
+        "name": "pCallType",
+        "itemType": "multiselect",
+        "configuration": {},
+        "params": {
+          "name": "pCallType"
+        },
+        "class": "col-md-2 col-lg-2 col-xs-2",
+        "affected": []
+      }
+    },
+    "512": {
+      "configuration": {
+        "type": "widget-container",
+        "name": "Week Total Outgoing Calls Usage",
+        "itemType": "",
+        "x": "0",
+        "y": "0",
+        "height": "2",
+        "width": "3",
+        "children": [
+          513
+        ]
+      }
+    },
+    "513": {
+      "configuration": {
+        "type": "widget-type-1",
+        "itemType": "widget-type-1",
+        "x": 0,
+        "y": 0,
+        "height": 3,
+        "width": 12,
+        "color": "bg-Teal-600",
+        "configuration": "dmFyIHJlc3VsdCA9IHsNCgkiY29sb3IiOiAiYmctZ3JleS0xMDAiLA0KCSJoZWFkZXIiOiAiIiwNCgkiZm9vdGVyIjogIiIsDQoJInNlcmllcyI6IFtdLA0KCSJzdWJ0aXRsZVRleHQiOiAiIiwNCgkiQ2hhcnREaXNwbGF5IjogIk5vbmUiDQp9DQp2YXIgZmluYWwgPSBkYXRhWzBdWzBdOw0KcmVzdWx0LmhlYWRlciA9ICI8ZGl2IGNsYXNzPSdyb3cnIHN0eWxlPSdiYWNrZ3JvdW5kLWNvbG9yOiAjREVDQTE1Oyc+PGRpdiBjbGFzcz0nY29sLW1kLTEyIGNvbC1sZy0xMiBjb2wteHMtMTInc3R5bGU9J2hlaWdodDoxMzBweDsnPjxkaXYgY2xhc3M9J3Jvdycgc3R5bGU9J2ZvbnQtc2l6ZToxNXB4O21hcmdpbi1sZWZ0OjlweDttYXJnaW4tdG9wOiAyOXB4OyBjb2xvcjp3aGl0ZSc+IFdlZWsgVG90YWwgT3V0Z29pbmcgQ2FsbHMgVXNhZ2UgPC9kaXY+PGRpdiBjbGFzcz0ncm93JyBzdHlsZT0nZm9udC1zaXplOjIycHg7IG1hcmdpbi10b3A6MTJweDttYXJnaW4tbGVmdDo2NXB4O2NvbG9yOndoaXRlJz4iICsgZmluYWwgKyAiJTwvZGl2PjwvZGl2PjwvZGl2PiI7DQpyZXR1cm4gcmVzdWx0Ow0K"
+      }
+    },
+    "515": {
+      "configuration": {
+        "type": "widget-container",
+        "name": "Week Average Call Duration By BTS ",
+        "itemType": "",
+        "x": "3",
+        "y": "0",
+        "height": "2",
+        "width": "3",
+        "children": [
+          516
+        ]
+      }
+    },
+    "516": {
+      "configuration": {
+        "type": "widget-type-1",
+        "itemType": "widget-type-1",
+        "x": 0,
+        "y": 0,
+        "height": 2,
+        "width": 12,
+        "color": "bg-Teal-600",
+        "configuration": "dmFyIHJlc3VsdCA9IHsNCgkiY29sb3IiOiAiYmctZ3JleS0xMDAiLA0KCSJoZWFkZXIiOiAiIiwNCgkiZm9vdGVyIjogIiIsDQoJInNlcmllcyI6IFtdLA0KCSJzdWJ0aXRsZVRleHQiOiAiIiwNCgkiQ2hhcnREaXNwbGF5IjogIk5vbmUiDQp9DQp2YXIgZmluYWwgPSBkYXRhWzBdWzBdOw0KcmVzdWx0LmhlYWRlciA9ICI8ZGl2IGNsYXNzPSdyb3cnIHN0eWxlPSdiYWNrZ3JvdW5kLWNvbG9yOiMwMUExREM7Jz48ZGl2IGNsYXNzPSdjb2wtbWQtMTIgY29sLWxnLTEyIGNvbC14cy0xMidzdHlsZT0naGVpZ2h0OjEzMHB4Oyc+PGRpdiBjbGFzcz0ncm93JyBzdHlsZT0nZm9udC1zaXplOjE1cHg7bWFyZ2luLWxlZnQ6LTRweDttYXJnaW4tdG9wOiAzNHB4OyBjb2xvcjp3aGl0ZSc+V2VlayBBdmVyYWdlIENhbGwgRHVyYXRpb24gQnkgQlRTPC9kaXY+PGRpdiBjbGFzcz0ncm93JyBzdHlsZT0nZm9udC1zaXplOjIxcHg7IG1hcmdpbi10b3A6N3B4O3RleHQtYWxpZ246IGNlbnRlcjtjb2xvcjp3aGl0ZSc+IiArIGZpbmFsICsgIjwvZGl2PjwvZGl2PjwvZGl2PiI7DQpyZXR1cm4gcmVzdWx0Ow=="
+      }
+    },
+    "517": {
+      "configuration": {
+        "type": "widget-container",
+        "name": "Week Total Revenue Units",
+        "itemType": "",
+        "x": "6",
+        "y": "0",
+        "height": "2",
+        "width": "3",
+        "children": [
+          518
+        ]
+      }
+    },
+    "518": {
+      "configuration": {
+        "type": "widget-type-1",
+        "itemType": "widget-type-1",
+        "x": 0,
+        "y": 0,
+        "height": 2,
+        "width": 12,
+        "color": "bg-Teal-600",
+        "configuration": "dmFyIHJlc3VsdCA9IHsNCgkiY29sb3IiOiAiYmctZ3JleS0xMDAiLA0KCSJoZWFkZXIiOiAiIiwNCgkiZm9vdGVyIjogIiIsDQoJInNlcmllcyI6IFtdLA0KCSJzdWJ0aXRsZVRleHQiOiAiIiwNCgkiQ2hhcnREaXNwbGF5IjogIk5vbmUiDQp9DQp2YXIgZmluYWwgPSBkYXRhWzBdWzBdOw0KcmVzdWx0LmhlYWRlciA9ICI8ZGl2IGNsYXNzPSdyb3cnIHN0eWxlPSdiYWNrZ3JvdW5kLWNvbG9yOiAjN0RCMDQzOyc+PGRpdiBjbGFzcz0nY29sLW1kLTEyIGNvbC1sZy0xMiBjb2wteHMtMTInc3R5bGU9J2hlaWdodDoxMzBweDsnPjxkaXYgY2xhc3M9J3Jvdycgc3R5bGU9J2ZvbnQtc2l6ZToxNXB4O21hcmdpbi1sZWZ0OjM1cHg7bWFyZ2luLXRvcDogMzJweDsgY29sb3I6d2hpdGUnPldlZWsgVG90YWwgUmV2ZW51ZSBVbml0cyA8L2Rpdj48ZGl2IGNsYXNzPSdyb3cnIHN0eWxlPSdmb250LXNpemU6MjFweDsgICAgdGV4dC1hbGlnbjogY2VudGVyOyBtYXJnaW4tdG9wOjdweDtjb2xvcjp3aGl0ZSc+IiArIGZpbmFsICsgIjwvZGl2PjwvZGl2PjwvZGl2PiI7DQpyZXR1cm4gcmVzdWx0Ow=="
+      }
+    },
+    "519": {
+      "configuration": {
+        "type": "widget-container",
+        "name": "Week Average Revenue Units By BTS ",
+        "itemType": "",
+        "x": "9",
+        "y": "0",
+        "height": "2",
+        "width": "3",
+        "children": [
+          520
+        ]
+      }
+    },
+    "520": {
+      "configuration": {
+        "type": "widget-type-1",
+        "itemType": "widget-type-1",
+        "x": 0,
+        "y": 0,
+        "height": 2,
+        "width": 12,
+        "color": "bg-Teal-600",
+        "configuration": "dmFyIHJlc3VsdCA9IHsNCgkiY29sb3IiOiAiYmctZ3JleS0xMDAiLA0KCSJoZWFkZXIiOiAiIiwNCgkiZm9vdGVyIjogIiIsDQoJInNlcmllcyI6IFtdLA0KCSJzdWJ0aXRsZVRleHQiOiAiIiwNCgkiQ2hhcnREaXNwbGF5IjogIk5vbmUiDQp9DQp2YXIgZmluYWwgPSBkYXRhWzBdWzBdOw0KcmVzdWx0LmhlYWRlciA9ICI8ZGl2IGNsYXNzPSdyb3cnIHN0eWxlPSdiYWNrZ3JvdW5kLWNvbG9yOiAjZmY5ODAwOyc+PGRpdiBjbGFzcz0nY29sLW1kLTEyIGNvbC1sZy0xMiBjb2wteHMtMTInc3R5bGU9J2hlaWdodDoxMzBweDsnPjxkaXYgY2xhc3M9J3Jvdycgc3R5bGU9J2ZvbnQtc2l6ZToxNXB4O21hcmdpbi1sZWZ0Oi00cHg7bWFyZ2luLXRvcDogMzRweDsgY29sb3I6d2hpdGUnPldlZWsgQXZlcmFnZSBSZXZlbnVlIFVuaXRzIEJ5IEJUUzwvZGl2PjxkaXYgY2xhc3M9J3Jvdycgc3R5bGU9J2ZvbnQtc2l6ZToyMXB4OyBtYXJnaW4tdG9wOjdweDt0ZXh0LWFsaWduOiBjZW50ZXI7Y29sb3I6d2hpdGUnPiIgKyBmaW5hbCArICI8L2Rpdj48L2Rpdj48L2Rpdj4iOw0KcmV0dXJuIHJlc3VsdDs="
+      }
+    },
+    "524": {
+      "configuration": {
+        "type": "multi-chart-element",
+        "name": "Revenue Units Breakup ",
+        "itemType": "",
+        "x": "0",
+        "y": "2",
+        "height": "6",
+        "width": "6",
+        "children": [
+          526
+        ]
+      }
+    },
+    "526": {
+      "configuration": {
+        "type": "chart-type-1",
+        "name": "Revenue Units Breakup",
+        "itemType": "chart-type-1",
+        "x": "0",
+        "y": "0",
+        "height": "5",
+        "width": "11",
+        "configuration": [
+          {
+            "categories": {
+              "columnMapping": 0
+            }
+          },
+          {
+            "series": {
+              "type": "column",
+              "name": "ISD",
+              "color": "#CDDC39",
+              "columnMapping": 1
+            }
+          },
+          {
+            "series": {
+              "type": "column",
+              "name": "Local",
+              "color": "#FF377C",
+              "columnMapping": 2
+            }
+          },
+          {
+            "series": {
+              "type": "column",
+              "name": "STD",
+              "color": "#8BC34A",
+              "columnMapping": 3
+            }
+          }
+        ]
+      }
+    },
+    "541": {
+      "configuration": {
+        "type": "multi-chart-element",
+        "name": "Revenue Units By District ",
+        "itemType": "",
+        "x": "6",
+        "y": "2",
+        "height": "6",
+        "width": "6",
+        "children": []
+      }
+    },
+    "555": {
+      "configuration": {
+        "type": "multi-chart-element",
+        "name": "BTS By Revenue Units ",
+        "itemType": "",
+        "x": "0",
+        "y": "8",
+        "height": "4",
+        "width": "6",
+        "children": [
+          556
+        ]
+      }
+    },
+    "556": {
+      "configuration": {
+        "type": "chart-type-1",
+        "name": "BTS By Revenue Units",
+        "itemType": "chart-type-1",
+        "x": "0",
+        "y": "0",
+        "height": "3",
+        "width": "11",
+        "configuration": [
+          {
+            "categories": {
+              "columnMapping": 0
+            }
+          },
+          {
+            "series": {
+              "type": "column",
+              "name": "Local",
+              "color": "#00BCD4",
+              "columnMapping": 1
+            }
+          },
+          {
+            "series": {
+              "type": "column",
+              "name": "STD",
+              "color": "orange",
+              "columnMapping": 2
+            }
+          },
+          {
+            "series": {
+              "type": "column",
+              "name": "ISD",
+              "color": "rgb(139, 195, 74)",
+              "columnMapping": 3
+            }
+          }
+        ]
+      }
+    },
+    "557": {
+      "configuration": {
+        "type": "multi-chart-element",
+        "name": "Cities By Revenue Units ",
+        "itemType": "",
+        "x": "6",
+        "y": "8",
+        "height": "4",
+        "width": "6",
+        "children": [
+          558
+        ]
+      }
+    },
+    "558": {
+      "configuration": {
+        "type": "chart-type-1",
+        "name": "Cities By Revenue Units",
+        "itemType": "chart-type-1",
+        "x": "0",
+        "y": "0",
+        "height": "3",
+        "width": "11",
+        "configuration": [
+          {
+            "categories": {
+              "columnMapping": 0
+            }
+          },
+          {
+            "series": {
+              "type": "area",
+              "name": "Revenue Units",
+              "color": "#F4E563",
+              "columnMapping": 1
+            }
+          }
+        ]
+      }
+    },
+    "559": {
+      "configuration": {
+        "type": "multi-chart-element",
+        "name": "BTS Daily Trend",
+        "itemType": "",
+        "x": "0",
+        "y": "12",
+        "height": "6",
+        "width": "12",
+        "children": [
+          560
+        ]
+      }
+    },
+    "560": {
+      "configuration": {
+        "type": "chart-type-1",
+        "name": "BTS Daily Trend",
+        "itemType": "chart-type-1",
+        "x": "0",
+        "y": "0",
+        "height": "5",
+        "width": "11",
+        "configuration": [
+          {
+            "categories": {
+              "columnMapping": 0
+            }
+          },
+          {
+            "series": {
+              "type": "spline",
+              "name": "Very Low",
+              "color": "#009688",
+              "yAxis": 0,
+              "columnMapping": 1
+            }
+          },
+          {
+            "series": {
+              "type": "spline",
+              "name": "Low",
+              "color": "#F44336",
+              "yAxis": 0,
+              "columnMapping": 2
+            }
+          },
+          {
+            "series": {
+              "type": "spline",
+              "name": "Medium",
+              "color": "#2196F3",
+              "yAxis": 0,
+              "columnMapping": 3
+            }
+          },
+          {
+            "series": {
+              "type": "spline",
+              "name": "High",
+              "color": "#FF9800",
+              "yAxis": 0,
+              "columnMapping": 4
+            }
+          },
+          {
+            "series": {
+              "type": "spline",
+              "name": "Very High",
+              "color": "#AFBDC5",
+              "yAxis": 0,
+              "columnMapping": 5
+            }
+          }
+        ]
+      }
+    }
+  },
+  "error": false
+};
